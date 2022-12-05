8b079fd28f395712d444d950497c83d09c296811
Ilia Yunakov <76445770+yunikeil@users.noreply.github.com>
2022-12-05 16:55:56 +0300

    -

--- configuration.py
+++ configuration.py
@@ -4,4 +4,7 @@ import os
 data_path: str = r'C:\Users\iyuna\source\repos\python\yunikeil\disBotWT\data'
 bot_id: int = 1037477633413349406
 
+ru_role_id: int = 12345
+en_role_id: int = 12345
+
 token: str = 'MTAzNzQ3NzYzMzQxMzM0OTQwNg.GYfQls.5wj55LV8mcPWTcHyz--Ip8O48ngO9D_iExZAWE'
